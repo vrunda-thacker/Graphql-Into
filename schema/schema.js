691e6570fec1fff8df42b4634896d7fb6d25cf3e
Paresh <paresh_mangukiya@jisto.com>
2018-10-17 18:12:47 +0530

    final commit for now

--- schema/schema.js
+++ schema/schema.js
@@ -7,7 +7,8 @@ const {
   GraphQLString,
   GraphQLInt,
   GraphQLSchema,
-  GraphQLList
+  GraphQLList,
+  GraphQLNonNull
 } = graphql;
 
 const CompanyType = new GraphQLObjectType({
@@ -79,7 +80,69 @@ const RootQuery = new GraphQLObjectType({
   }
 });
 
+const mutation = new GraphQLObjectType({
+  name: 'Mutation',
+  fields: {
+    addUser: {
+      type: UserType,
+      args: {
+        firstName:{
+          type: new GraphQLNonNull(GraphQLString)
+        },
+        age: {
+          type: new GraphQLNonNull(GraphQLInt)
+        },
+        companyId: {
+          type: GraphQLString
+        }
+      },
+      resolve (parentValue, {firstName, age}) {
+        return axios.post('http://localhost:3000/users', {
+            firstName,
+            age
+        }).then(rsp => rsp.data);
+      }
+    },
+    deleteUser: {
+      type: UserType,
+      args: {
+        id: {
+          type: new GraphQLNonNull(GraphQLString)
+        }
+      },
+      resolve (parentValue, {id}) {
+        return axios.delete(`http://localhost:3000/users/${id}`).then(rsp => rsp.data);
+      }
+    },
+    updateUser: {
+      type: UserType,
+      args: {
+        id: {
+          type: new GraphQLNonNull((GraphQLString))
+        },
+        firstName: {
+          type: GraphQLString
+        },
+        age: {
+          type: GraphQLInt
+        },
+        companyId: {
+          type: GraphQLString
+        }
+      },
+      resolve(parentValue, args) {
+        return axios.patch(`http://localhost:3000/users/${args.id}`, {
+          firstName: args.firstName,
+          age: args.age,
+          companyId: args.companyId
+        }).then(rsp => rsp.data);
+      }
+    }
+  }
+});
+
 
 module.exports = new GraphQLSchema({
-  query: RootQuery
+  query: RootQuery,
+  mutation
 });
